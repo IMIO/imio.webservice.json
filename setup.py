87295b1284b0bb0239acc4ca972477b213833c23
Martin Peeters <martin.peeters@affinitic.be>
2014-04-07 11:00:26 +0200

    Update package metadata

--- setup.py
+++ setup.py
@@ -15,30 +15,33 @@ requires = [
     'pyramid_debugtoolbar',
     'waitress',
     'warlock',
-    ]
+]
 
-setup(name='imio.webservice.json',
-      version='0.0',
-      description='imio.webservice.json',
-      long_description=README + '\n\n' + CHANGES,
-      classifiers=[
+setup(
+    name='imio.webservice.json',
+    version='0.0',
+    description='imio.webservice.json',
+    long_description=README + '\n\n' + CHANGES,
+    classifiers=[
+        "Environment :: Web Environment",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
         "Framework :: Pyramid",
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
-        ],
-      author='',
-      author_email='',
-      url='',
-      keywords='web pyramid pylons',
-      packages=find_packages(),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=requires,
-      tests_require=requires,
-      test_suite="imiowebservicejson",
-      entry_points="""\
-      [paste.app_factory]
-      main = imiowebservicejson:main
-      """,
-      )
+    ],
+    author='IMIO',
+    author_email='support@imio.be',
+    url='https://github.com/imio/',
+    keywords='web pyramid pylons',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=requires,
+    tests_require=requires,
+    test_suite="imiowebservicejson",
+    entry_points="""\
+    [paste.app_factory]
+    main = imiowebservicejson:main
+    """,
+)
