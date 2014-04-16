42e51b5a946c3ba801cb6b5bd5c9857f520e6a96
Martin Peeters <martin.peeters@affinitic.be>
2014-04-16 11:26:53 +0200

    Improve the version comparison

--- imiowebservicejson/predicates.py
+++ imiowebservicejson/predicates.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 
+import operator
+import re
+from distutils.version import StrictVersion
+
 
 class ImplementPredicate(object):
 
@@ -26,8 +30,30 @@ class VersionPredicate(object):
     phash = text
 
     def __call__(self, event):
-        version = event.request.context.version
+        current_version = StrictVersion(event.request.context.version)
         for expression in self.versions:
-            if eval(version + expression) is False:
+            op, version = get_operator_and_version(expression)
+            if op(current_version, version) is False:
                 return False
         return True
+
+
+def get_operator_and_version(expression):
+    """ Return the operator and the version from an expression e.g >= 2.0.1 """
+
+    op_string = re.search('[>=<!]{0,2}', expression).group()
+    version = expression.replace(op_string, '').strip()
+    return get_operator(op_string), StrictVersion(version)
+
+
+def get_operator(string):
+    """ Return the associated python operator function """
+    op_dict = {'>': operator.gt,
+               '<': operator.lt,
+               '>=': operator.ge,
+               '=>': operator.ge,
+               '<=': operator.le,
+               '=<': operator.le,
+               '==': operator.eq,
+               '!=': operator.ne}
+    return op_dict.get(string, operator.eq)
