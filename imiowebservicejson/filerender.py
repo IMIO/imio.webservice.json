01f7434b9681412ef21fb92353a0ac5728cd3f99
Martin Peeters <martin.peeters@affinitic.be>
2014-10-16 10:19:22 +0200

    Add a missing file and fix the file route parameters

---
+++ imiowebservicejson/filerender.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from imio.dataexchange.db.mappers.file import File
+
+
+class FileRender(object):
+
+    def __init__(self, request):
+        self.dms_file = File.first(
+            client_id=request.matchdict.get('client_id'),
+            external_id=request.matchdict.get('external_id'),
+        )
+
+    def render(self):
+        f = open(self.filepath, 'r')
+        return f.read()
+
+    @property
+    def filepath(self):
+        return self.dms_file.filepath
