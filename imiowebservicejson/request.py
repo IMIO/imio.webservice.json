273c9eba1d6b9c5e80e4c56d317019bae607c30f
Martin Peeters <martin.peeters@affinitic.be>
2014-11-27 17:34:29 +0100

    Rename the exchanges for the request and response webservices

--- imiowebservicejson/request.py
+++ imiowebservicejson/request.py
@@ -7,6 +7,7 @@ from imio.amqp import BaseSingleMessageConsumer
 class SinglePublisher(BaseSingleMessagePublisher):
     logger_name = 'request_notifier'
     log_file = 'request_notifier.log'
+    exchange = 'ws.request'
 
     def get_routing_key(self, message):
         return message.client_id
@@ -35,4 +36,4 @@ class RequestFile(object):
 class SingleConsumer(BaseSingleMessageConsumer):
     logger_name = 'request_notifier'
     log_file = 'request_notifier.log'
-    exchange = 'ws.connection'
+    exchange = 'ws.response'
