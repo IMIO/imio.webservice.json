aebd41f1dfc9b8279285bd30e7b84a9d1a145a67
Martin Peeters <martin.peeters@affinitic.be>
2018-12-05 11:04:22 +0100

    Add swagger service

--- imiowebservicejson/__init__.py
+++ imiowebservicejson/__init__.py
@@ -44,6 +44,7 @@ def main(global_config, **settings):
     )
     # adds cornice
     config.include("cornice")
+    config.include('cornice_swagger')
 
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
