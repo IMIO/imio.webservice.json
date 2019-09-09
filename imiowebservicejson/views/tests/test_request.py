31ddb4ec92cccd8877e4c5a1cf39950f72e5f2e3
Martin Peeters <martin.peeters@affinitic.be>
2019-09-09 14:05:04 +0200

    ISI-47: Add more tests for new caching parameters

--- imiowebservicejson/views/tests/test_request.py
+++ imiowebservicejson/views/tests/test_request.py
@@ -44,6 +44,63 @@ class TestRequestService(TestAppBaseTestCase):
     @mock.patch("hashlib.md5", mock.Mock(return_value=FakeMD5("ABC")))
     @mock.patch("imiowebservicejson.request.SinglePublisher.start")
     @mock.patch("imiowebservicejson.request.SinglePublisher.add_message")
+    def test_post_duplicate_request_get(self, mocked_start, mocked_add_message):
+        params = {
+            "client_id": "CLI",
+            "application_id": "APP",
+            "request_type": "GET",
+            "path": "/test",
+            "parameters": {},
+        }
+        results = [
+            self.app.post("/request", params=params),
+            self.app.post("/request", params=params),
+        ]
+        self.assertEqual(1, mocked_add_message.call_count)
+        self.assertEqual(1, mocked_start.call_count)
+        self.assertListEqual(["200 OK", "200 OK"], [e.status for e in results])
+        for element in results:
+            self.assertEqual(
+                {
+                    u"request_id": u"ABC",
+                    u"client_id": u"CLI",
+                    u"application_id": u"APP",
+                },
+                json.loads(element.body),
+            )
+
+    @mock.patch("hashlib.md5", mock.Mock(return_value=FakeMD5("ABC")))
+    @mock.patch("imiowebservicejson.request.SinglePublisher.start")
+    @mock.patch("imiowebservicejson.request.SinglePublisher.add_message")
+    def test_post_duplicate_ignored_cache_request_get(self, mocked_start, mocked_add_message):
+        params = {
+            "client_id": "CLI",
+            "application_id": "APP",
+            "request_type": "GET",
+            "path": "/test",
+            "parameters": {},
+            "ignore_cache": True,
+        }
+        results = [
+            self.app.post("/request", params=params),
+            self.app.post("/request", params=params),
+        ]
+        self.assertEqual(1, mocked_add_message.call_count)
+        self.assertEqual(1, mocked_start.call_count)
+        self.assertListEqual(["200 OK", "200 OK"], [e.status for e in results])
+        for element in results:
+            self.assertEqual(
+                {
+                    u"request_id": u"ABC",
+                    u"client_id": u"CLI",
+                    u"application_id": u"APP",
+                },
+                json.loads(element.body),
+            )
+
+    @mock.patch("hashlib.md5", mock.Mock(return_value=FakeMD5("ABC")))
+    @mock.patch("imiowebservicejson.request.SinglePublisher.start")
+    @mock.patch("imiowebservicejson.request.SinglePublisher.add_message")
     def test_post_without_expiration(self, mocked_start, mocked_add_message):
         """ Test the post_request when there is a result but without expiration date """
         params = {
@@ -102,6 +159,44 @@ class TestRequestService(TestAppBaseTestCase):
         record = RequestTable.first(uid="CLI-APP-ABC")
         self.assertEqual(datetime(2019, 1, 1, 12, 5, 0, 0), record.expiration_date)
         self.assertEqual("response", record.response)
+
+    @mock.patch("hashlib.md5", mock.Mock(return_value=FakeMD5("ABC")))
+    @mock.patch(
+        "imiowebservicejson.utils.now",
+        mock.Mock(return_value=datetime(2019, 1, 1, 12, 0, 0)),
+    )
+    @mock.patch("imiowebservicejson.request.SinglePublisher.start")
+    @mock.patch("imiowebservicejson.request.SinglePublisher.add_message")
+    def test_post_ignore_cache(self, mocked_start, mocked_add_message):
+        """ Test the post_request when there is a result, the expiration date
+        is in the future and the ignore_cache parameter is used"""
+        params = {
+            "client_id": "CLI",
+            "application_id": "APP",
+            "request_type": "GET",
+            "path": "/test",
+            "ignore_cache": True,
+            "parameters": {},
+        }
+        request_record = RequestTable(
+            uid="CLI-APP-ABC",
+            expiration_date=datetime(2019, 1, 1, 12, 5, 0, 0),
+            response="response",
+        )
+        session = temporary_session(DBSession.bind)
+        session.add(request_record)
+        session.commit()
+        result = self.app.post("/request", params=params)
+        self.assertTrue(mocked_add_message.called)
+        self.assertTrue(mocked_start.called)
+        self.assertEqual("200 OK", result.status)
+        self.assertEqual(
+            {u"request_id": u"ABC", u"client_id": u"CLI", u"application_id": u"APP"},
+            json.loads(result.body),
+        )
+        record = RequestTable.first(uid="CLI-APP-ABC")
+        self.assertIsNone(record.expiration_date)
+        self.assertIsNone(record.response)
 
     @mock.patch("hashlib.md5", mock.Mock(return_value=FakeMD5("ABC")))
     @mock.patch(
